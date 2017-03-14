d5cee71fc4b9fb858d957f957d2a3fcc359eadac
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2017-03-15 00:27:32 +0100

    Testing tournament view.

--- src/app/+tournament/tournament.component.spec.ts
+++ src/app/+tournament/tournament.component.spec.ts
@@ -1,25 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
-import { compose } from '@ngrx/core/compose';
-
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { TournamentComponent } from './tournament.component';
+import { State } from '../reducers';
+import * as players from '../actions/players.actions';
+import * as queue from '../actions/queue.actions';
 import { initialState } from '../reducers/players-queue.reducer';
 
 describe('TournamentComponent', () => {
   let component: TournamentComponent;
   let fixture: ComponentFixture<TournamentComponent>;
+  let store: any;
+  let spyDispatch: jasmine.Spy;
+  let spySelect: jasmine.Spy;
+
+  beforeEach(() => {
+    store = {
+      dispatch: () => {},
+      select: () => {},
+    };
+    spyDispatch = spyOn(store, 'dispatch');
+    spySelect = spyOn(store, 'select').and.callFake(
+      (selector: any) => Observable.of({players: {
+        ...initialState,
+        entities: {
+          ['test1']: {lives: 2, initialLives: 3},
+        },
+        queue: ['test1'],
+      }}).map(selector),
+    );
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         CommonModule,
-        StoreModule.provideStore({}),
+        RouterTestingModule,
       ],
-      declarations: [ TournamentComponent ]
+      declarations: [ TournamentComponent ],
+      providers: [
+        {provide: Store, useValue: store}
+      ],
     })
     .compileComponents();
   }));
@@ -30,7 +54,31 @@ describe('TournamentComponent', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should reset the tournament', () => {
+    component.startTournament();
+
+    expect(spyDispatch).toHaveBeenCalledWith(jasmine.objectContaining({
+      type: queue.ActionTypes.SHUFFLE,
+    }));
+    expect(spyDispatch).toHaveBeenCalledWith(jasmine.objectContaining({
+      type: players.ActionTypes.RESET_LIVES,
+      payload: 3,
+    }));
+  });
+
+  it('should dispatch the match result', () => {
+    component.matchFinished(<any>{id: 'test1'}, <any>{id: 'test2'});
+
+    expect(spyDispatch).toHaveBeenCalledWith(jasmine.objectContaining({
+      type: queue.ActionTypes.RESULT,
+      payload: {
+        winner: jasmine.objectContaining({id: 'test1'}),
+        loser: jasmine.objectContaining({id: 'test2'}),
+      },
+    }));
   });
 });
