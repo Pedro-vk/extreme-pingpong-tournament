2ba7603d3216bc8f1846ebc1e23458b455c272bc
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2017-04-13 01:08:47 +0200

    Added the basic author and version information.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+
+declare var require: any;
 
 @Component({
   selector: 'ept-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  githubRepositoryUrl: string;
+  projectVersion: string;
+  projectAuthor: string;
+
+  ngOnInit(): void {
+    this.getGithubRepositoryUrl();
+  }
+
+  getGithubRepositoryUrl(): void {
+    try {
+      const packageInfo = require('../../package.json');
+
+      this.githubRepositoryUrl = packageInfo.repository.url;
+      this.projectVersion = packageInfo.version;
+      this.projectAuthor = packageInfo.author.split(' <')[0];
+    } catch (e) { }
+  }
+}
